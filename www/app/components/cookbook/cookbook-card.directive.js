ac76d699226c6a1d3e61503d7a5fcd5c4ae668e4
Oleg Cherkasskiy <oleg201119@yandex.com>
2017-06-22 03:09:21 +0300

    remove comment

--- www/app/components/cookbook/cookbook-card.directive.js
+++ www/app/components/cookbook/cookbook-card.directive.js
@@ -6,9 +6,7 @@ angular.module('foodapp.directive')
     replace: true,
     scope: {},
     templateUrl: 'app/components/cookbook/cookbook-card.directive.html',
-    link: function(scope, elem, attrs) {
-      console.log(attrs);
-
+    link: function(scope, elem, attrs) {      
       scope.banner = attrs.banner;
       scope.title = attrs.title;
     }
